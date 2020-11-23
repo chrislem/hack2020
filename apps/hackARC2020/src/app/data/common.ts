183e53870d1a45a4cbc41025120770884ea322a6
Hubert <JeanGuillaume.HUBERT@finastra.com>
2020-11-24 00:19:30 +0100

    removed spec.ts. added maturities and ARR_indexes

--- apps/hackARC2020/src/app/data/common.ts
+++ apps/hackARC2020/src/app/data/common.ts
@@ -17,10 +17,37 @@ export var periodicity = [
     '1w',
     '1m',
     '3m',
-    '6m'
+    '6m',
+    '12m'
 ];
 
 export var amoType_list = [
 'Bullet',
 'Constant Capital'
 ];
+
+export var maturities = [
+'1m',
+'3m',
+'6m',
+'1y',
+'2y',
+'3y',
+'4y',
+'5y',
+'6y',
+'7y',
+'8y',
+'9y',
+'10y',
+'15y',
+'20y'
+];
+
+export var ARR_Indexes = [
+    'ESTER',
+    'SOFR',
+    'SONIA',
+    'SARON',
+    'TONAR'
+]
