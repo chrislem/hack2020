9c479397f20267d6b259aaebdfebff6a1679a4ca
Hubert <JeanGuillaume.HUBERT@finastra.com>
2020-11-24 15:07:06 +0100

    common

--- apps/hackARC2020/src/app/data/common.ts
+++ apps/hackARC2020/src/app/data/common.ts
@@ -3,7 +3,8 @@ export var currencies = [
     'EUR',
     'USD',
     'GBP',
-    'JPY'
+    'JPY',
+    'CHF'
 ];
 
 export var basis = [
@@ -51,3 +52,38 @@ export var ARR_Indexes = [
     'SARON',
     'TONAR'
 ]
+
+export var mapCurrencyARR = new Map([
+    ["EUR","ESTER"],
+    ["USD","SOFR"],
+    ["GBP","SONIA"],
+    ["CHF","SARON"],
+    ["JPY","TONAR"]
+  ])
+
+export var mapPeriodicity = new Map([
+    ["1","1d"],
+    ["2","1w"],
+    ["3","1m"],
+    ["4","3m"],
+    ["5","6m"],
+    ["6","12m"]]);
+
+    export var mapMaturity = new Map([
+        ["1","1m"],
+        ["2","3m"],
+        ["3","6m"],
+        ["4","1y"],
+        ["5","2y"],
+        ["6","3y"],
+        ["7","4y"],
+        ["8","5y"],
+        ["9","6y"],
+        ["10","7y"],
+        ["11","8y"],
+        ["12","9y"],   
+        ["13","10y"],
+        ["14","15y"],
+        ["15","20y"]
+  
+    ]);
